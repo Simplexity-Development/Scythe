63f43abbb698c566245b60bf0cff34ab1035297e
Rhythmic System <mc.adhdmc@gmail.com>
2025-03-01 23:35:15 -0800

    committing

--- src/main/java/simplexity/scythe/handling/Util.java
+++ src/main/java/simplexity/scythe/handling/Util.java
@@ -1,6 +1,5 @@
 package simplexity.scythe.handling;
 
-import com.destroystokyo.paper.ParticleBuilder;
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import net.coreprotect.CoreProtectAPI;
 import net.kyori.adventure.key.Key;
@@ -10,6 +9,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
@@ -19,6 +19,7 @@ import simplexity.scythe.config.ConfigHandler;
 import simplexity.scythe.config.Message;
 import simplexity.scythe.hooks.CoreProtectHook;
 
+import java.util.HashMap;
 import java.util.Set;
 
 public class Util {
@@ -35,12 +36,15 @@ public class Util {
     private final CoreProtectAPI coreProtectAPI = CoreProtectHook.getInstance().getCoreProtect();
 
     public boolean harvestAllowedWithCurrentTool(Player player) {
-        return !ConfigHandler.getInstance().doesRightClickHarvestRequireTool() || requiredToolUsed(player);
+        return !ConfigHandler.getInstance().rightClickHarvestRequiresTool() || requiredToolUsed(player);
     }
 
     public boolean replantAllowedWithCurrentTool(Player player, boolean rightClick) {
-        if (rightClick && !ConfigHandler.getInstance().shouldRequireToolLeftClickReplant()) return true;
-        else if (!rightClick && !ConfigHandler.getInstance().shouldRequireToolRightClickReplant()) return true;
+        if (rightClick) {
+            if (!ConfigHandler.getInstance().rightClickReplantRequiresTool()) return true;
+            return requiredToolUsed(player);
+        }
+        if (!ConfigHandler.getInstance().leftClickReplantRequiresTool()) return true;
         return requiredToolUsed(player);
     }
 
@@ -58,31 +62,50 @@ public class Util {
     }
 
     @SuppressWarnings("DataFlowIssue")
-    public void handleDurability(Player player) {
-        if (!ConfigHandler.getInstance().doesHarvestUseDurability()) return;
+    public boolean toolHasEnoughDurability(Player player) {
         ItemStack item = player.getInventory().getItemInMainHand();
         if (!item.hasData(DataComponentTypes.MAX_DAMAGE) || !item.hasData(DataComponentTypes.DAMAGE)) return;
         int currentDamage = item.getData(DataComponentTypes.DAMAGE);
         int maxDamage = item.getData(DataComponentTypes.MAX_DAMAGE);
-        if (ConfigHandler.getInstance().shouldPreventToolBreak() &&
-            (maxDamage - currentDamage <= ConfigHandler.getInstance().getMinimumDurability())){
+        if (ConfigHandler.getInstance().preventToolBreaking() &&
+            (maxDamage - currentDamage <= ConfigHandler.getInstance().getMinimumDurability())) {
             player.sendMessage(Scythe.getMiniMessage().deserialize(Message.YOUR_TOOL_IS_ALMOST_BROKEN.getMessage()));
-            return;
+            return false;
         }
+        return true;
+    }
+
+    public void handleDurability(Player player) {
+        if (!ConfigHandler.getInstance().harvestUsesDurability()) return;
+        ItemStack item = player.getInventory().getItemInMainHand();
+        int currentDamage = item.getData(DataComponentTypes.DAMAGE);
         item.setData(DataComponentTypes.DAMAGE, currentDamage + 1);
     }
 
     public boolean hasSeeds(Player player, Material seedMaterial) {
-        return player.getInventory().contains(ItemStack.of(seedMaterial));
+        return player.getInventory().containsAtLeast(ItemStack.of(seedMaterial), 1);
     }
 
-    public void handleSeeds(Player player, boolean rightClick, BlockData blockData) {
-        if (rightClick && !ConfigHandler.getInstance().doesRightClickReplantRequireSeeds()) return;
-        if (!rightClick && !ConfigHandler.getInstance().doesLeftClickReplantRequireSeeds()) return;
-        int location = player.getInventory().first(ItemStack.of(blockData.getPlacementMaterial()));
-        ItemStack seeds = player.getInventory().getItem(location);
-        if (seeds == null) return;
+    /*
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+     */
+    public boolean seedsHandledProperly(Player player, Material seedMaterial) {
+        if (!ConfigHandler.getInstance().replantRequiresSeeds()) return true;
+        Integer seedLocation = getSeedLocation(player, seedMaterial);
+        if (seedLocation == null) return false;
+        ItemStack seeds = player.getInventory().getItem(seedLocation);
+        if (seeds == null) return false;
         seeds.subtract(1);
+        return true;
+    }
+
+    private Integer getSeedLocation(Player player, Material seedMaterial) {
+        ItemStack defaultSeeds = ItemStack.of(seedMaterial);
+        HashMap<Integer, ? extends ItemStack> slotItems = player.getInventory().all(seedMaterial);
+        for (Integer slot : slotItems.keySet()) {
+            if (slotItems.get(slot).asOne().equals(defaultSeeds)) return slot;
+        }
+        return null;
     }
 
     public void logCoreProtectRemoval(Player player, Block block) {
@@ -100,9 +123,9 @@ public class Util {
         coreProtectAPI.logPlacement(player.getName(), location, block.getType(), blockData);
     }
 
-    public void playHarvestEffects(Block block) {
+    public void playHarvestEffects(Block block, BlockData originalBlockData) {
         handleSound(block);
-        handleParticles(block);
+        handleParticles(block, originalBlockData);
     }
 
     public void handleSound(Block block) {
@@ -114,19 +137,57 @@ public class Util {
         location.getWorld().playSound(location, sound, SoundCategory.BLOCKS, volume, pitch);
     }
 
-    private void handleParticles(Block block) {
+    private void handleParticles(Block block, BlockData originalBlockData) {
         if (!ConfigHandler.getInstance().showBreakParticles()) return;
         Location location = block.getLocation().toCenterLocation();
-        BlockData blockData = block.getBlockData();
-        int particleCount = ConfigHandler.getInstance().getParticleCount();
         Particle particle = ConfigHandler.getInstance().getConfigParticle();
-        ParticleBuilder particleBuilder = new ParticleBuilder(particle);
-        if (particle.getDataType().equals(BlockData.class)) {
-            particleBuilder.data(blockData);
+        if (particle.getDataType().isInstance(BlockData.class)) {
+            spawnParticlesWithBlockData(location, originalBlockData, particle);
+        } else {
+            spawnParticlesWithoutBlockData(location, particle);
         }
-        particleBuilder.location(location);
-        particleBuilder.count(particleCount);
-        particleBuilder.spawn();
+
+
     }
+
+    private void spawnParticlesWithBlockData(Location location, BlockData blockData, Particle particle) {
+        int particleCount = ConfigHandler.getInstance().getParticleCount();
+        double particleSpread = ConfigHandler.getInstance().getParticleSpread();
+
+        World world = location.getWorld();
+        for (int i = 0; i < particleCount; i++) {
+            double offsetX = (Math.random() - 0.5) * particleSpread;
+            double offsetY = (Math.random() - 0.5) * particleSpread;
+            double offsetZ = (Math.random() - 0.5) * particleSpread;
+            world.spawnParticle(
+                    particle,
+                    location.getX() + offsetX,
+                    location.getY() + offsetY,
+                    location.getZ() + offsetZ,
+                    1,
+                    blockData
+            );
+        }
+    }
+
+    private void spawnParticlesWithoutBlockData(Location location, Particle particle) {
+        int particleCount = ConfigHandler.getInstance().getParticleCount();
+        double particleSpread = ConfigHandler.getInstance().getParticleSpread();
+
+        World world = location.getWorld();
+        for (int i = 0; i < particleCount; i++) {
+            double offsetX = (Math.random() - 0.5) * particleSpread;
+            double offsetY = (Math.random() - 0.5) * particleSpread;
+            double offsetZ = (Math.random() - 0.5) * particleSpread;
+            world.spawnParticle(
+                    particle,
+                    location.getX() + offsetX,
+                    location.getY() + offsetY,
+                    location.getZ() + offsetZ,
+                    1
+            );
+        }
+    }
+
 
 }
